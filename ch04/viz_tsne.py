b762102d64fde3d1c85250cafdbb34a441cc8ac6
kyle <kyle@wrtn.io>
2025-03-18 16:17:19 +0900

    update viz

--- ch04/viz_tsne.py
+++ ch04/viz_tsne.py
@@ -12,16 +12,40 @@ def load_model(pkl_file):
 
 
 # t-SNE 변환 및 시각화
-def visualize_tsne(pkl_file, output_file="cbow_tsne.png", num_words=200, seed=1000):
+def visualize_tsne(
+    pkl_file,
+    output_file="cbow_tsne.png",
+    query_words=[],
+    analogy_words=[],
+    analogy_word_keys=[],
+    seed=1000,
+):
     params = load_model(pkl_file)
     word_vecs = params["word_vecs"]
     id_to_word = params["id_to_word"]
+    word_to_id = params["word_to_id"]
     np.random.seed(seed)
 
-    # 일부 단어만 선택 (너무 많으면 시각화가 어려움)
-    selected_indices = np.random.choice(len(word_vecs), num_words, replace=False)
-    selected_vectors = word_vecs[selected_indices]
-    selected_labels = [id_to_word[i] for i in selected_indices]
+    # 단어 벡터 추출
+    query_indices = [word_to_id[word] for word in query_words if word in word_to_id]
+    analogy_indices = [word_to_id[word] for word in analogy_words if word in word_to_id]
+    key_indices = [
+        word_to_id[word]
+        for pair in analogy_word_keys
+        for word in pair
+        if word in word_to_id
+    ]
+
+    all_indices = query_indices + analogy_indices + key_indices
+    selected_vectors = word_vecs[all_indices]
+    selected_labels = [id_to_word[i] for i in all_indices]
+
+    # 색상 지정
+    selected_colors = (
+        ["blue"] * len(query_indices)
+        + ["red"] * len(analogy_indices)
+        + ["green"] * len(key_indices)
+    )
 
     # t-SNE 적용
     tsne = TSNE(n_components=2, perplexity=30, random_state=42)
@@ -29,10 +53,13 @@ def visualize_tsne(pkl_file, output_file="cbow_tsne.png", num_words=200, seed=10
 
     # 시각화
     plt.figure(figsize=(12, 8))
-    plt.scatter(reduced_vectors[:, 0], reduced_vectors[:, 1], alpha=0.7)
-
-    # 단어 라벨 추가
     for i, label in enumerate(selected_labels):
+        plt.scatter(
+            reduced_vectors[i, 0],
+            reduced_vectors[i, 1],
+            color=selected_colors[i],
+            alpha=0.7,
+        )
         plt.annotate(
             label, (reduced_vectors[i, 0], reduced_vectors[i, 1]), fontsize=9, alpha=0.7
         )
@@ -42,8 +69,115 @@ def visualize_tsne(pkl_file, output_file="cbow_tsne.png", num_words=200, seed=10
     plt.show()
 
 
+query_words = [
+    "you",
+    "year",
+    "car",
+    "toyota",
+    "yourself",
+    "i",
+    "we",
+    "anything",
+    "anybody",
+    "your",
+    "weird",
+    "somebody",
+    "earlier",
+    "month",
+    "quarter",
+    "week",
+    "summer",
+    "spring",
+    "decade",
+    "period",
+    "luxury",
+    "cars",
+    "auto",
+    "mazda",
+    "truck",
+    "motor",
+    "honda",
+    "nissan",
+    "infiniti",
+    "lexus",
+]
+
+analogy_word_keys = [
+    ["king", "man"],
+    ["take", "took"],
+    ["car", "cars"],
+    ["good", "better"],
+]
+
+analogy_words = [
+    "king",
+    "man",
+    "queen",
+    "take",
+    "took",
+    "go",
+    "went",
+    "car",
+    "cars",
+    "child",
+    "children",
+    "good",
+    "better",
+    "bad",
+    "worse",
+    "peace",
+    "freeway",
+    "bikers",
+    "teacher",
+    "born",
+    "a.m",
+    "fbi",
+    "woman",
+    "brother",
+    "answers",
+    "pricings",
+    "non-u.s.",
+    "eurodollars",
+    "'re",
+    "were",
+    "went",
+    "was",
+    "feet",
+    "yards",
+    "ran",
+    "rape",
+    "incest",
+    "daffynition",
+    "bond-equivalent",
+    "non-violent",
+    "pregnant",
+    "adjustable",
+    "always",
+    "rather",
+    "more",
+    "less",
+    "greater",
+    "faster",
+    "impressive",
+    "swings",
+    "vary",
+    "ever",
+]
+
 # 사용 예시 (파일명 변경 필요)
 pkl_file = "cbow_params.pkl"  # 또는 'skipgram_params.pkl'
-visualize_tsne(pkl_file, "cbow_tsne.png", 40)
+visualize_tsne(
+    pkl_file,
+    "cbow_tsne.png",
+    query_words=query_words,
+    analogy_words=analogy_words,
+    analogy_word_keys=analogy_word_keys,
+)
 pkl_file = "skip_gram_params.pkl"
-visualize_tsne(pkl_file, "skip_gram_tsne.png", 40)
+visualize_tsne(
+    pkl_file,
+    "skip_gram_tsne.png",
+    query_words=query_words,
+    analogy_words=analogy_words,
+    analogy_word_keys=analogy_word_keys,
+)
