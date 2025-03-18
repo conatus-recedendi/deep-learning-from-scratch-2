74d6bd71dcc1a159153f81feb80da56f739b6373
kyle <kyle@wrtn.io>
2025-03-18 09:57:01 +0900

    feat: update

--- ch04/train.py
+++ ch04/train.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 import numpy as np
 from common import config
+
 # GPU에서 실행하려면 아래 주석을 해제하세요(CuPy 필요).
 # ===============================================
-# config.GPU = True
+config.GPU = True
 # ===============================================
 import pickle
 from common.trainer import Trainer
@@ -14,30 +16,66 @@ from cbow import CBOW
 from skip_gram import SkipGram
 from common.util import create_contexts_target, to_cpu, to_gpu
 from dataset import ptb
+import wandb
 
 
-# 하이퍼파라미터 설정
-window_size = 5
-hidden_size = 100
-batch_size = 100
-max_epoch = 10
+wandb.init(
+    project="Word2Vec",
+    name="Negative_All_Sample_CBOW",
+    config={
+        "parameters": {
+            "seed": {"value": 1000},
+            "gradient_descent": {"value": "Adam"},
+            "learning_rate": {"value": 0.001},
+            "epochs": {"value": 10},
+            "batch_size": {"value": 100},
+            "model": {"value": "Negative_All_Sample_CBOW"},
+            "model_params": {
+                "value": {
+                    "hidden_size": 100,
+                    "window_size": 5,
+                    "power": 0.75,
+                },
+            },
+            "dataset": {"value": "PTB"},
+            "gpu": {"value": config.GPU},
+            # "batch_norm": {"value": False},
+            # "weight_decay_lambda": {"value": 0},
+            # "dataset": {"value": ""},
+            # "activation": {"value": "relu"},
+            # "weight_init_std": {"value": "he"},
+            # "dropout": {"value": 0.15},
+        },
+    },
+)
+
+np.random.seed(wandb.config.seed)
 
 # 데이터 읽기
-corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus, word_to_id, id_to_word = ptb.load_data("train")
 vocab_size = len(word_to_id)
 
-contexts, target = create_contexts_target(corpus, window_size)
+contexts, target = create_contexts_target(
+    corpus, window_size=wandb.config.model_params["window_size"]
+)
 if config.GPU:
     contexts, target = to_gpu(contexts), to_gpu(target)
 
 # 모델 등 생성
-model = CBOW(vocab_size, hidden_size, window_size, corpus)
+model = CBOW(
+    vocab_size,
+    hidden_size=wandb.config.model_params["hidden_size"],
+    window_size=wandb.config.model_params["window_size"],
+    corpus=corpus,
+)
 # model = SkipGram(vocab_size, hidden_size, window_size, corpus)
 optimizer = Adam()
 trainer = Trainer(model, optimizer)
 
 # 학습 시작
-trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.fit(
+    contexts, target, max_epoch=wandb.config.epochs, batch_size=wandb.config.batch_size
+)
 trainer.plot()
 
 # 나중에 사용할 수 있도록 필요한 데이터 저장
@@ -45,9 +83,9 @@ word_vecs = model.word_vecs
 if config.GPU:
     word_vecs = to_cpu(word_vecs)
 params = {}
-params['word_vecs'] = word_vecs.astype(np.float16)
-params['word_to_id'] = word_to_id
-params['id_to_word'] = id_to_word
-pkl_file = 'cbow_params.pkl'  # or 'skipgram_params.pkl'
-with open(pkl_file, 'wb') as f:
+params["word_vecs"] = word_vecs.astype(np.float16)
+params["word_to_id"] = word_to_id
+params["id_to_word"] = id_to_word
+pkl_file = "cbow_params.pkl"  # or 'skipgram_params.pkl'
+with open(pkl_file, "wb") as f:
     pickle.dump(params, f, -1)
--- common/trainer.py
+++ common/trainer.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 import numpy
 import time
 import matplotlib.pyplot as plt
 from common.np import *  # import numpy as np
-from common.util import clip_grads
+from common.util import clip_grads, cast_to_single_value
 
 
 class Trainer:
@@ -27,18 +28,21 @@ class Trainer:
         start_time = time.time()
         for epoch in range(max_epoch):
             # 뒤섞기
+            avg_loss = 0
             idx = numpy.random.permutation(numpy.arange(data_size))
             x = x[idx]
             t = t[idx]
 
             for iters in range(max_iters):
-                batch_x = x[iters*batch_size:(iters+1)*batch_size]
-                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+                batch_x = x[iters * batch_size : (iters + 1) * batch_size]
+                batch_t = t[iters * batch_size : (iters + 1) * batch_size]
 
                 # 기울기 구해 매개변수 갱신
                 loss = model.forward(batch_x, batch_t)
                 model.backward()
-                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                params, grads = remove_duplicate(
+                    model.params, model.grads
+                )  # 공유된 가중치를 하나로 모음
                 if max_grad is not None:
                     clip_grads(grads, max_grad)
                 optimizer.update(params, grads)
@@ -49,20 +53,36 @@ class Trainer:
                 if (eval_interval is not None) and (iters % eval_interval) == 0:
                     avg_loss = total_loss / loss_count
                     elapsed_time = time.time() - start_time
-                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f'
-                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    print(
+                        "| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f"
+                        % (
+                            self.current_epoch + 1,
+                            iters + 1,
+                            max_iters,
+                            elapsed_time,
+                            avg_loss,
+                        )
+                    )
                     self.loss_list.append(float(avg_loss))
                     total_loss, loss_count = 0, 0
 
             self.current_epoch += 1
 
+            wandb.log(
+                {
+                    "Loss": cast_to_single_value(avg_loss),
+                    # "train_acc": cast_to_single_value(acc_train),
+                    # "test_acc": cast_to_single_value(acc_test),
+                }
+            )
+
     def plot(self, ylim=None):
         x = numpy.arange(len(self.loss_list))
         if ylim is not None:
             plt.ylim(*ylim)
-        plt.plot(x, self.loss_list, label='train')
-        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
-        plt.ylabel('손실')
+        plt.plot(x, self.loss_list, label="train")
+        plt.xlabel("반복 (x" + str(self.eval_interval) + ")")
+        plt.ylabel("손실")
         plt.show()
 
 
@@ -76,12 +96,14 @@ class RnnlmTrainer:
         self.current_epoch = 0
 
     def get_batch(self, x, t, batch_size, time_size):
-        batch_x = np.empty((batch_size, time_size), dtype='i')
-        batch_t = np.empty((batch_size, time_size), dtype='i')
+        batch_x = np.empty((batch_size, time_size), dtype="i")
+        batch_t = np.empty((batch_size, time_size), dtype="i")
 
         data_size = len(x)
         jump = data_size // batch_size
-        offsets = [i * jump for i in range(batch_size)]  # 배치에서 각 샘플을 읽기 시작하는 위치
+        offsets = [
+            i * jump for i in range(batch_size)
+        ]  # 배치에서 각 샘플을 읽기 시작하는 위치
 
         for time in range(time_size):
             for i, offset in enumerate(offsets):
@@ -90,8 +112,16 @@ class RnnlmTrainer:
             self.time_idx += 1
         return batch_x, batch_t
 
-    def fit(self, xs, ts, max_epoch=10, batch_size=20, time_size=35,
-            max_grad=None, eval_interval=20):
+    def fit(
+        self,
+        xs,
+        ts,
+        max_epoch=10,
+        batch_size=20,
+        time_size=35,
+        max_grad=None,
+        eval_interval=20,
+    ):
         data_size = len(xs)
         max_iters = data_size // (batch_size * time_size)
         self.time_idx = 0
@@ -109,7 +139,9 @@ class RnnlmTrainer:
                 # 기울기를 구해 매개변수 갱신
                 loss = model.forward(batch_x, batch_t)
                 model.backward()
-                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                params, grads = remove_duplicate(
+                    model.params, model.grads
+                )  # 공유된 가중치를 하나로 모음
                 if max_grad is not None:
                     clip_grads(grads, max_grad)
                 optimizer.update(params, grads)
@@ -120,8 +152,16 @@ class RnnlmTrainer:
                 if (eval_interval is not None) and (iters % eval_interval) == 0:
                     ppl = np.exp(total_loss / loss_count)
                     elapsed_time = time.time() - start_time
-                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 퍼플렉서티 %.2f'
-                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, ppl))
+                    print(
+                        "| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 퍼플렉서티 %.2f"
+                        % (
+                            self.current_epoch + 1,
+                            iters + 1,
+                            max_iters,
+                            elapsed_time,
+                            ppl,
+                        )
+                    )
                     self.ppl_list.append(float(ppl))
                     total_loss, loss_count = 0, 0
 
@@ -131,17 +171,17 @@ class RnnlmTrainer:
         x = numpy.arange(len(self.ppl_list))
         if ylim is not None:
             plt.ylim(*ylim)
-        plt.plot(x, self.ppl_list, label='train')
-        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
-        plt.ylabel('퍼플렉서티')
+        plt.plot(x, self.ppl_list, label="train")
+        plt.xlabel("반복 (x" + str(self.eval_interval) + ")")
+        plt.ylabel("퍼플렉서티")
         plt.show()
 
 
 def remove_duplicate(params, grads):
-    '''
+    """
     매개변수 배열 중 중복되는 가중치를 하나로 모아
     그 가중치에 대응하는 기울기를 더한다.
-    '''
+    """
     params, grads = params[:], grads[:]  # copy list
 
     while True:
@@ -157,16 +197,23 @@ def remove_duplicate(params, grads):
                     params.pop(j)
                     grads.pop(j)
                 # 가중치를 전치행렬로 공유하는 경우(weight tying)
-                elif params[i].ndim == 2 and params[j].ndim == 2 and \
-                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                elif (
+                    params[i].ndim == 2
+                    and params[j].ndim == 2
+                    and params[i].T.shape == params[j].shape
+                    and np.all(params[i].T == params[j])
+                ):
                     grads[i] += grads[j].T
                     find_flg = True
                     params.pop(j)
                     grads.pop(j)
 
-                if find_flg: break
-            if find_flg: break
+                if find_flg:
+                    break
+            if find_flg:
+                break
 
-        if not find_flg: break
+        if not find_flg:
+            break
 
     return params, grads
