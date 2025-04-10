3f35e1bd40fb6a14dee6898c10217c163a076868
kyle <kyle@wrtn.io>
2025-04-11 05:53:57 +0900

    change sampler

--- ch04/negative_sampling_layer_our.py
+++ ch04/negative_sampling_layer_our.py
@@ -36,13 +36,14 @@ class EmbeddingDot:
 class UnigramSampler:
     def __init__(self, corpus, power, sample_size):
         self.sample_size = sample_size
+        self.power = power
+        self.corpus = corpus
         self.vocab_size = None
         self.word_p = None
 
         counts = collections.Counter()
         for word_id in corpus:
             counts[word_id] += 1
-
         vocab_size = len(counts)
         self.vocab_size = vocab_size
 
@@ -54,27 +55,9 @@ class UnigramSampler:
         self.word_p /= np.sum(self.word_p)
 
     def get_negative_sample(self, target):
-        batch_size = target.shape[0]
-        if not config.GPU:
-            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
-
-            for i in range(batch_size):
-                p = self.word_p.copy()
-                target_idx = target[i]
-                p[target_idx] = 0
-                p /= p.sum()
-                negative_sample[i, :] = np.random.choice(
-                    self.vocab_size, size=self.sample_size, replace=False, p=p
-                )
-        else:
-            # GPU(cupy）로 계산할 때는 속도를 우선한다.
-            # 부정적 예에 타깃이 포함될 수 있다.
-            negative_sample = np.random.choice(
-                self.vocab_size,
-                size=(batch_size, self.sample_size),
-                p=self.word_p,
-            )
-
+        negative_sample = np.random.choice(
+            self.vocab_size, (target.shape[0], self.sample_size), p=self.word_p
+        )
         return negative_sample
 
 
